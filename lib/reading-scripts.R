3158ed04d8ad91219b37bf3686f3438aae274744
Maria Hoerhold <mhoerhol@dmawi.de>
2019-11-08 10:27:18 +0100

    Add function to read Arctic2k data

--- lib/reading-scripts.R
+++ lib/reading-scripts.R
@@ -6,6 +6,23 @@
 #' @param Input path
 #' @param read csv.
 #' @return dataframe
+
 readNGT<-function(path = "data/NGT2012_AnnualMean_FINAL.csv") {
   read.csv(path, header = TRUE)
+}
+
+
+readArctic2k<-function(path = "data/Reconstruction_Arc2kv1.1.1.csv") {
+  
+  # skip unneeded data columns
+  colClasses <- c(rep(NA, 4), rep("NULL", 6))
+  
+  col.names <- c("Year", "TempAnomaly", "2SigmaLow", "2SigmaHigh")
+  
+  dat <- read.csv(path, header = TRUE, colClasses = colClasses)
+  
+  colnames(dat) <- col.names
+  
+  return(dat)
+  
 }
