ef88d7eda43148e11754c210c4e8258e6e3f65aa
Thomas Münch <tmuench@awi.de>
2019-11-11 11:47:11 +0100

    add function to calculate anomaly time series.

---
+++ lib/makeAnomalies.R
@@ -0,0 +1,12 @@
+##' Anomaly time series
+##'
+##' Calculate anomaly time series relative to a given reference period.
+##' @param data numeric vector (time series)
+##' @param age the age scale of \code{data}
+##' @param reference.period numeric vector of ages of the reference period
+##' @return numeric vector with the anomaly time series
+##' @author Thomas Münch
+makeAnomalies <- function(data, age, reference.period = 1990 : 1961) {
+
+  data - mean(data[match(reference.period, age)])
+}
