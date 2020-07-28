df3487b926f3a379d9fa8639bbbf945d9ae52ce7
Thomas Münch <tmuench@awi.de>
2020-07-28 18:04:51 +0200

    restructure and use more sensible file names.

--- lib/data.R
+++ lib/data.R
@@ -198,3 +198,64 @@ countRecords <- function(ngt) {
              n = apply(ngt, MARGIN = 1, function(x) sum(!is.na(x))) - 1)
 
 }
+
+#' Anomaly time series
+#'
+#' Calculate anomaly time series relative to the mean of a given reference
+#' period.
+#'
+#' @param data a data set for which to calculate the anomalies; can be a single
+#'   numeric vector or a data frame or matrix of vectors. In the latter case,
+#'   the first column can be used to provide the age scale of the data; else use
+#'   the \code{age} parameter for this.
+#' @param age provide the age scale of \code{data} here if it is not supplied
+#'   in the latter parameter itself.
+#' @param reference.period numeric vector with the ages of the reference period.
+#' @param hasAgeColumn logical; do the supplied \code{data} contain an age
+#'   column as the first column? Setting this to \code{FALSE} but leaving
+#'   \code{age} as \code{NULL} will cause an error.
+#' @return numeric vector, matrix or data frame with the anomaly time series.
+#' @author Thomas Münch
+#'
+makeAnomalies <- function(data, age = NULL, reference.period = 1990 : 1961,
+                          hasAgeColumn = TRUE) {
+
+  if (is.null(age) & !hasAgeColumn) {
+    stop("Age scale information missing.", call. = FALSE)
+  }
+
+  if (hasAgeColumn) {
+    if (is.null(dim(data))) {
+      stop("'data' is not a matrix or data frame.", call. = FALSE)
+    }
+    if (ncol(data) < 2) {
+      stop("'data' only has 1 column; need at least 2.", call. = FALSE)
+    }
+    colNames <- colnames(data)
+    Year <- data[, 1]
+    data <- data[, -1]
+  } else {
+    Year <- age
+  }
+
+  i <- match(reference.period, Year)
+
+  if (is.null(dim(data))) {
+
+    result <- data - mean(data[i], na.rm = TRUE)
+
+  } else {
+
+    result <- apply(data, 2, function(x) {
+      x - mean(x[i], na.rm = TRUE)})
+
+  }
+
+  if (hasAgeColumn) {
+    result <- data.frame(Year, result)
+    colnames(result) <- colNames
+
+  }
+
+  return(result)
+}
--- lib/maths.R
+++ lib/maths.R
@@ -117,3 +117,101 @@ filterData <-function(data, window = 5, method = 2, hasAgeColumn = TRUE) {
   return(result)
   
 }
+
+#' Estimate isotope slopes
+#'
+#' Estimate the linear trend slopes in an isotope record within a given
+#' moving window.
+#'
+#' @param x a data frame with two columns with the time scale as the first and
+#'   the isotope values as the second column.
+#' @param window an integer value giving the size of the moving window within
+#'   which the linear trend slopes are estimated; must be an odd number.
+#' @return a data frame with two columns 'Year' and 'slope' with the time scale
+#'   as the first and the slope values as the second column.
+#' @author Thomas Münch
+#'
+estimateSlopes <- function(x, window = 21) {
+
+  if (window < 3) stop("No sensible estimation window size.")
+  if ((window %% 2) == 0) stop("Odd estimation window size expected.")
+  
+  slopes <- rep(NA, nrow(x))
+  window.half <- (window - 1) / 2
+  for (i in (window.half + 1) : (nrow(x) - window.half)) {
+
+    j <- (i - window.half) : (i + window.half)
+
+    slopes[i] <- coefficients(lm(x[j, 2] ~ x[j, 1]))[2]
+
+  }
+
+  res <- data.frame(Year = x[, 1], slope = slopes)
+  return(res)
+
+}
+
+#' Calculate standard error
+#'
+#' Calculate the standard error of a data vector, i.e. its standard deviation
+#' divided by the square root of the number of data points.
+#'
+#' @param x numeric vector for which to calculate the standard error.
+#' @param na.rm a logical value indicating whether \code{NA} values should be
+#'   stripped before the computation proceeds.
+#' @return the standard error of \code{x}.
+#' @author Maria Hoerhold
+#'
+sdError <- function(x, na.rm = FALSE) {
+  sd(x, na.rm = na.rm) / sqrt(length(x))
+}
+
+#' Statistics of the overlap period
+#'
+#' Deliver the statistics of the overlap period between the old and the
+#' new (re-drilled) isotope record at a certain site.
+#'
+#' @param data data frame with the NGT and associated oxygen isotope records.
+#' @param site character string of the site ID for which to calculate the
+#'   overlap statistics; must match the list of sites for which re-drilled
+#'   records are available.
+#' @return a nested list with two elements:
+#'   * "dat": a list of two data frames with the full data for the paired
+#'     records at the requested site and the data for the overlap period.
+#'   * "stat": a list of six elements with statistical information about the
+#'     records' overlap; including start and end year of overlap period, mean
+#'     isotope values in the overlap period, the difference of these mean
+#'     values, the correlation for the overlap period, and the standard error of
+#'     the mean values in the overlap period.
+#' @author Thomas Münch, Maria Hoerhold
+calculateOverlapStatistics <- function(data, site = "B18") {
+
+  if (!site %in% c("B18", "B21", "B23", "B26", "NGRIP", "stack"))
+    stop("Unsuitable site requested.")
+
+  pairID <- c("Year", site, paste(site, "12", sep = "_"))
+
+  pairData <- data[, pairID]
+  overlap <- na.omit(pairData)
+  meanOverlap <- colMeans(overlap[, -1])
+  sdErrorOverlap <- apply(overlap[, -1], 2, sdError)
+  
+  output <- list(
+
+    dat  = list(pairData = pairData, overlapData = overlap),
+    stat = data.frame(
+      corePair          = site,
+      startOverlap      = min(overlap$Year),
+      endOverlap        = max(overlap$Year),
+      meanOverlapOld    = meanOverlap[1],
+      meanOverlapNew    = meanOverlap[2],
+      sdErrorOverlapOld = sdErrorOverlap[1],
+      sdErrorOverlapNew = sdErrorOverlap[2],
+      diffMeanOverlap   = diff(meanOverlap),
+      corrOverlap       = cor(overlap[, 2], overlap[, 3]),
+      row.names = NULL)
+  )
+  
+  return(output)
+  
+}
