4388214c86686f5a73709aafe212ee971d56d690
Thomas Münch <tmuench@awi.de>
2022-04-22 23:30:31 +0200

    use plotting function for NGT vs. MAR.

--- lib/plotting.R
+++ lib/plotting.R
@@ -433,6 +433,95 @@ plot.NGT.Arctic2k <- function(filter.window = 11,
 
 }
 
+#' Produce a plot comparing NGT-2012 and MAR time series
+#'
+#' @param filter.window single integer giving the size of the running mean
+#'   window to use for filtering the isotope and Arctic2k data; defaults to 11
+#'   (years).
+#' @author Thomas Münch
+#'
+plot.NGT.MAR <- function(filter.window = 11) {
+
+  if (filter.window < 1) {
+    stop("Running mean filter window needs to be >= 1.", call. = FALSE)
+  }
+
+  if ((filter.window %% 2) == 0) {
+    warning("Running mean filter window should be odd.", call. = FALSE)
+  }
+
+  # ----------------------------------------------------------------------------
+  # Load time series data
+
+  permil2temperature.mid <- 1 / 0.67 # Greenland spatial slope
+  permil2temperature.low <- 1 / 1.1  # Masson-Delmotte et al. (2015)
+  permil2temperature.hig <- 2.1      # Vinther et al. (2009)
+
+  filteredStackedNGT <- processNGT() %>%
+    filterData(window = filter.window) %>%
+    stackNGT()
+
+  filteredStackedTemperatureNGT.mid <- filteredStackedNGT %>%
+    dplyr::mutate(stack = permil2temperature.mid * stack)
+  filteredStackedTemperatureNGT.low <- filteredStackedNGT %>%
+    dplyr::mutate(stack = permil2temperature.low * stack)
+  filteredStackedTemperatureNGT.hig <- filteredStackedNGT %>%
+    dplyr::mutate(stack = permil2temperature.hig * stack)
+
+  filteredMAR <- readMAR() %>%
+    filterData(window = filter.window)
+
+  # ----------------------------------------------------------------------------
+  # Make plots
+
+  xlab  <- "Year CE"
+  ylab1 <- grfxtools::LabelAxis("Temperature anomaly", unit = "celsius")
+  ylab2 <- grfxtools::LabelAxis("Melt runoff anomaly", unit = "Gt",
+                                unit.type = "trend", time.unit = "yr")
+
+  xlim <- c(1871, 2011)
+  ylim1 <- c(-2.5, 3)
+  ylim2 <- c(-55, 300)
+
+  x1 <- 2030
+  y1 <- 125
+
+  col <- c("black", "#d95f02", "#7570b3")
+
+  plot(filteredStackedTemperatureNGT.mid, type = "n", axes = FALSE,
+       xlab = "", ylab = "", xlim = xlim, ylim = ylim1, xaxs = "i")
+
+  lines(filteredStackedTemperatureNGT.mid, lwd = 2, col = col[1])
+
+  axis(1)
+  axis(2)
+  mtext(xlab, 1, 3.5, cex = par()$cex.lab)
+  mtext(ylab1, 2, 3, cex = par()$cex.lab, las = 0)
+
+  grfxtools::Polyplot(filteredStackedTemperatureNGT.mid$Year,
+                      filteredStackedTemperatureNGT.low$stack,
+                      filteredStackedTemperatureNGT.hig$stack,
+                      col = col[1], alpha = 0.2)
+  lines(filteredStackedTemperatureNGT.low, lwd = 1, col = col[1])
+  lines(filteredStackedTemperatureNGT.hig, lwd = 1, col = col[1])
+
+  lines(filteredMAR$Year, filteredMAR$t2m, lwd = 2, col = col[2])
+
+  par(new = TRUE)
+
+  plot(filteredMAR$Year, filteredMAR$melt, type = "l", axes = FALSE,
+       col = col[3], lwd = 2, xlim = xlim, ylim = ylim2, xaxs = "i",
+       xlab = "", ylab = "")
+
+  axis(4)
+  text(x1, y1, ylab2, srt = -90, xpd = NA, cex = par()$cex.lab * par()$cex)
+
+  legend("topleft",
+         c("NGT-2012 temperature", "MAR3.5 temperature", "MAR3.5 melt runoff"),
+         lty = 1, lwd = 2, col = col, bty = "n")
+
+}
+
 #' Produce spectrum plot of NGT-2012 versus Arctic2k
 #'
 #' @param filter.window single integer giving the size of the running mean
